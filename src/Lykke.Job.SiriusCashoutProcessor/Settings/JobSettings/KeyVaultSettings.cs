506fffe8a9ff4760738a333ed824909201db4aaf
Dmitry Samodov <dmitry.samodov@swisschain.io>
2021-05-25 20:48:52 +0300

    LM-1041: sign withdrawal request

---
+++ src/Lykke.Job.SiriusCashoutProcessor/Settings/JobSettings/KeyVaultSettings.cs
@@ -0,0 +1,10 @@
+namespace Lykke.Job.SiriusCashoutProcessor.Settings.JobSettings
+{
+    public class KeyVaultSettings
+    {
+        public string VaultBaseUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PrivateKeyName { get; set; }
+    }
+}
