55628333c2692b0ead3d04a749252434f20da825
Dmitry Samodov <dmitry.samodov@swisschain.io>
2021-05-25 20:48:52 +0300

    LM-1041: sign withdrawal request

--- src/Lykke.Job.SiriusCashoutProcessor/Services/StartupManager.cs
+++ src/Lykke.Job.SiriusCashoutProcessor/Services/StartupManager.cs
@@ -3,29 +3,32 @@ using Antares.Sdk.Services;
 using Common.Log;
 using Lykke.Common.Log;
 using Lykke.Cqrs;
+using Lykke.Job.SiriusCashoutProcessor.DomainServices;
 
 namespace Lykke.Job.SiriusCashoutProcessor.Services
 {
     public class StartupManager : IStartupManager
     {
+        private readonly PrivateKeyService _privateKeyService;
         private readonly ICqrsEngine _cqrsEngine;
         private readonly ILog _log;
 
         public StartupManager(
             ILogFactory logFactory,
+            PrivateKeyService privateKeyService,
             ICqrsEngine cqrsEngine
         )
         {
+            _privateKeyService = privateKeyService;
             _cqrsEngine = cqrsEngine;
             _log = logFactory.CreateLog(this);
         }
 
         public async Task StartAsync()
         {
+            await _privateKeyService.InitAsync();
             _cqrsEngine.StartSubscribers();
             _cqrsEngine.StartProcesses();
-
-            await Task.CompletedTask;
         }
     }
 }
--- src/Lykke.Job.SiriusCashoutProcessor/Workflow/CommandHandlers/CashoutCommandHandler.cs
+++ src/Lykke.Job.SiriusCashoutProcessor/Workflow/CommandHandlers/CashoutCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using Common;
 using Common.Log;
@@ -6,7 +7,8 @@ using JetBrains.Annotations;
 using Lykke.Common.Log;
 using Lykke.Cqrs;
 using Lykke.Job.SiriusCashoutProcessor.Contract.Commands;
-using Lykke.Job.SiriusCashoutProcessor.Domain;
+using Lykke.Job.SiriusCashoutProcessor.DomainServices;
+using Swisschain.Extensions.Encryption;
 using Swisschain.Sirius.Api.ApiClient;
 using Swisschain.Sirius.Api.ApiContract.Withdrawal;
 
@@ -16,17 +18,22 @@ namespace Lykke.Job.SiriusCashoutProcessor.Workflow.CommandHandlers
     {
         private readonly long _brokerAccountId;
         private readonly IApiClient _siriusApiClient;
+        private readonly PrivateKeyService _privateKeyService;
         private readonly ILog _log;
+        private readonly AsymmetricEncryptionService _encryptionService;
 
         public CashoutCommandHandler(
             long brokerAccountId,
             IApiClient siriusApiClient,
+            PrivateKeyService privateKeyService,
             ILogFactory logFactory
             )
         {
             _brokerAccountId = brokerAccountId;
             _siriusApiClient = siriusApiClient;
+            _privateKeyService = privateKeyService;
             _log = logFactory.CreateLog(this);
+            _encryptionService = new AsymmetricEncryptionService();
         }
 
         [UsedImplicitly]
@@ -34,23 +41,28 @@ namespace Lykke.Job.SiriusCashoutProcessor.Workflow.CommandHandlers
         {
             _log.Info("Got cashout command", context: $"command: {command.ToJson()}" );
 
+            var document = new WithdrawalDocument
+            {
+                BrokerAccountId = _brokerAccountId,
+                UserNativeId = command.ClientId.ToString(),
+                WithdrawalReferenceId = command.OperationId.ToString(),
+                AssetId = command.SiriusAssetId,
+                Amount = command.Amount,
+                DestinationDetails = new WithdrawalDocument.WithdrawalDestinationDetails
+                {
+                    Address = command.Address,
+                    Tag = command.Tag ?? string.Empty
+                }
+            }.ToJson();
+
+            var signatureBytes = _encryptionService.GenerateSignature(Encoding.UTF8.GetBytes(document),  _privateKeyService.GetPrivateKey());
+            var signature = Convert.ToBase64String(signatureBytes);
 
             var result = await _siriusApiClient.Withdrawals.ExecuteAsync(new WithdrawalExecuteRequest
             {
                 RequestId = $"{_brokerAccountId}_{command.OperationId}",
-                Document = new WithdrawalDocument
-                {
-                    BrokerAccountId = _brokerAccountId,
-                    UserNativeId = command.ClientId.ToString(),
-                    WithdrawalReferenceId = command.OperationId.ToString(),
-                    AssetId = command.SiriusAssetId,
-                    Amount = command.Amount,
-                    DestinationDetails = new DestinationDetails
-                    {
-                        Address = command.Address,
-                        Tag = command.Tag ?? string.Empty
-                    }
-                }.ToJson()
+                Document = document,
+                Signature = signature
             });
 
             var techData = result.ResultCase == WithdrawalExecuteResponse.ResultOneofCase.Body
