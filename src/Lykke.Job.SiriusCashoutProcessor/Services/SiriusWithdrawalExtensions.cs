9044de97f2c0f6e11e1fe0875710abecce404b78
Andrey Volkov <netsky@bk.ru>
2024-01-29 18:23:13 +0100

    LM-3105 Map userNativeId from properties of withdrawal (#16)
    * LM-3105 Map userNativeId from properties of withdrawal
    
    * LM-3105 Fix PR notes
    
    ---------
    
    Co-authored-by: Andrey Volkov <avolkov.dev.10x@gmail.com>

---
+++ src/Lykke.Job.SiriusCashoutProcessor/Services/SiriusWithdrawalExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Swisschain.Sirius.Api.ApiClient.Utils.Builders.V2;
+using Swisschain.Sirius.Api.ApiContract.Withdrawal;
+
+namespace Lykke.Job.SiriusCashoutProcessor.Services;
+
+public static class SiriusWithdrawalExtensions
+{
+    public static string GetUserNativeId(this WithdrawalResponse withdrawal)
+    {
+        if (withdrawal == null)
+            throw new ArgumentNullException(nameof(withdrawal));
+
+        if(withdrawal.Properties.TryGetValue(KnownProperties.UserId, out var userId))
+        {
+            return userId;
+        }
+
+        //fallback to old property
+        return withdrawal.UserNativeId;
+    }
+
+    public static string GetAccountReferenceId(this WithdrawalResponse withdrawal)
+    {
+        
+        if (withdrawal == null)
+            throw new ArgumentNullException(nameof(withdrawal));
+
+        if(withdrawal.Properties.TryGetValue(KnownProperties.WalletId, out var walletId))
+        {
+            return walletId;
+        }
+
+        //fallback to old property
+        return withdrawal.AccountReferenceId;
+    }
+}
